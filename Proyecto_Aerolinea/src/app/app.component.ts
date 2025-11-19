0fb6ae2f607d258ccc5979994cc544655d2a51b2
Juan Hernandez <juanjhernandezs1@gmail.com>
2025-11-19 01:27:33 -0500

    Feat: Implementacion y mejora de estilo by J2

--- Proyecto_Aerolinea/src/app/app.component.ts
+++ Proyecto_Aerolinea/src/app/app.component.ts
@@ -1,13 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { HeaderComponent } from "./header/header.component"
-import { CardVuelosComponent } from './card-vuelos/card-vuelos.component';
-import { ToolbarBasicDemo } from './toolbar/toolbar.component';
-import { CardPublicitarioComponent } from './card-publicitario/card-publicitario.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, CardVuelosComponent, CardPublicitarioComponent, ToolbarBasicDemo],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- Proyecto_Aerolinea/src/app/home-page/home-page.component.ts
+++ Proyecto_Aerolinea/src/app/home-page/home-page.component.ts
@@ -2,12 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from '../header/header.component';
 import { CardVuelosComponent } from '../card-vuelos/card-vuelos.component';
-import { Toolbar } from 'primeng/toolbar';
 import { CardPublicitarioComponent } from '../card-publicitario/card-publicitario.component';
 
 @Component({
   selector: 'app-home-page',
-  imports: [RouterOutlet, HeaderComponent, CardVuelosComponent, Toolbar, CardPublicitarioComponent],
+  imports: [RouterOutlet, HeaderComponent, CardVuelosComponent, CardPublicitarioComponent],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.css'
 })
